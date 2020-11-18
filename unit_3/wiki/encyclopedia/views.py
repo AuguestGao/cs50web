093763f1ca0b3c9d3362e2e59d1f005487174a09
AG <AuLucian@users.noreply.github.com>
2020-11-17 22:01:58 -0500

    backup before pset1

--- unit_3/wiki/encyclopedia/views.py
+++ unit_3/wiki/encyclopedia/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 from . import util
 
+#class NewEntryForm(forms.Form):
+ #   entry = forms.CharField(label="new entry")
+
 
 def index(request):
+
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+#def entry(request):
+ #   if request.method
