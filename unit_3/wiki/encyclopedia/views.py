3448faf4be5b5c6268bf41ecc6fd17f3b047cfb4
AG <knightauguest@gmail.com>
2020-11-18 16:46:46 -0500

    index.html functional

--- unit_3/wiki/encyclopedia/views.py
+++ unit_3/wiki/encyclopedia/views.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from . import util
-
+import markdown2
 #class NewEntryForm(forms.Form):
  #   entry = forms.CharField(label="new entry")
 
 
 def index(request):
-
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 
-#def entry(request):
- #   if request.method
+def entry(request, title):
+    entry_md = util.get_entry(title)
+    if entry_md:
+        #use markdown2 to convert .md to .html
+        entry_html = markdown2.markdown(entry_md)
+        return render(request, "encyclopedia/entry.html", {
+            "entry_html": entry_html
+        })
+    return HttpResponse("404 No such an entry found")
