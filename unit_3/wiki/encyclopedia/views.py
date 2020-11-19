0b98adc909dc7bf86043efae5241844f54fecd16
AG <knightauguest@gmail.com>
2020-11-19 17:50:14 -0500

    backup commit, 2020-11-19 5:50pm

--- unit_3/wiki/encyclopedia/views.py
+++ unit_3/wiki/encyclopedia/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
 from . import util
+
 import markdown2
-#class NewEntryForm(forms.Form):
- #   entry = forms.CharField(label="new entry")
 
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+            "entries": util.list_entries()
     })
 
 
 def entry(request, title):
     entry_md = util.get_entry(title)
+
     if entry_md:
         #use markdown2 to convert .md to .html
         entry_html = markdown2.markdown(entry_md)
